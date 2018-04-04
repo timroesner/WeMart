0971b85330f672ff2ddc5ec47b11518b346295a9
rajmakda <makdaraj7@gmail.com>
2018-04-04 11:33:59 -0700

    items from db added to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import HorizontalScroll from './HorizontalScroll';
 import Header from './header';
-import { withRouter } from "react-router-dom";
+import { withRouter } from 'react-router-dom';
+import {db} from '../db'
 
 class Home extends Component {
   constructor(props) {
@@ -9,58 +10,43 @@ class Home extends Component {
 
     this.state = {
       departmentItems: [],
-      savingsItems: [{
-        name: 'Cream Cheese',
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Philly_cream_cheese.jpg/1200px-Philly_cream_cheese.jpg',
-        price: '3.79',
-        sale: '2.49',
-        quantity: '8 oz'
-      },
-      {
-        name: 'Apples',
-        image: 'https://www.organicfacts.net/wp-content/uploads/2013/05/Apple4.jpg',
-        price: '3.79',
-        sale: '2.49',
-        quantity: '8 oz'
-      },
-      {
-        name: 'Bagel',
-        image: 'https://d3cizcpymoenau.cloudfront.net/images/24017/SFS_Bagel_V2-14.jpg',
-        price: '3.79',
-        sale: '2.49',
-        quantity: '8 oz'
-      },
-      {
-        name: 'Apples',
-        image: 'https://www.organicfacts.net/wp-content/uploads/2013/05/Apple4.jpg',
-        price: '3.79',
-        sale: '2.49',
-        quantity: '8 oz'
-      },
-      {
-        name: 'Apples',
-        image: 'https://www.organicfacts.net/wp-content/uploads/2013/05/Apple4.jpg',
-        price: '3.79',
-        sale: '2.49',
-        quantity: '8 oz'
-      },
-      {
-        name: 'Apples',
-        image: 'https://www.organicfacts.net/wp-content/uploads/2013/05/Apple4.jpg',
-        price: '3.79',
-        sale: '2.49',
-        quantity: '8 oz'
-      },
-      {
-        name: 'Apples',
-        image: 'https://www.organicfacts.net/wp-content/uploads/2013/05/Apple4.jpg',
-        price: '3.79',
-        sale: '2.49',
-        quantity: '8 oz'
-      }],
+      savingsItems: [],
       historyItems: []
     }
+    //this.getItemsFromDB();
   }
+
+  componentDidMount = () =>  {
+    // Get the dynamoDB database
+        var dynamodb = db;
+
+        // Get the table whose name is "item"
+        var params = {
+            TableName: "item"
+        };
+
+        dynamodb.scan(params, (err, data) => {
+            if (err) {console.log(err, err.stack)} // an error occurred
+            else{
+                data.Items.forEach((element) => {
+
+                    //TODO Clean this up into a one liner.
+                    let departmentid = element.departmentid.N;
+                    let image = element.image.S;
+                    let itemid = (element.itemid.S);
+                    let name = (element.name.S);
+                    let price = (element.price.N);
+                    let quantity = (element.quantity.S);
+                    let sale = (element.sale.N);
+
+                    let testItem = {itemid: itemid, name: name, departmentid: departmentid, image: image, price: price, quantity: quantity, sale: sale};
+                    this.setState({
+                        savingsItems: [...this.state.savingsItems, testItem]
+                    });
+                });
+            }
+        });
+      }
 
   render() {
     return (
