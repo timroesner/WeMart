19d13ad475682f4481c2911fa9f3215907776c79
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-04-05 00:21:59 -0700

    Add Mobile Layout

--- src/Item.js
+++ src/Item.js
@@ -66,23 +66,6 @@ class Item extends Component {
 
 	render() {
 
-		const imgDiv = {
-			margin: '3%',
-			width: '45%',
-			height: '500px',
-			float: 'left'
-		}
-
-		const astext = {
-		    background:'none',
-		    border:'none',
-		    marginLeft:'10%',
-		    width: '80px',
-		    padding:'0',
-		    fontSize: '1em',
-			textAlign: 'center',
-		}
-
 		const price = true ? 
 		(<p style={{marginTop: '5%', color: 'red', fontSize: '1.8em'}}>
 			<span style={{color: 'black', textDecoration: 'line-through', webkitTextDecorationColor: 'red'}}>$27.99</span>
@@ -91,44 +74,111 @@ class Item extends Component {
 		(<p style={{marginTop: '5%', fontSize: '1.8em'}}>
 			$3.79
 		</p>) 
+	if(window.innerWidth < 550) {
+		const astext = {
+		    background:'none',
+		    border:'none',
+		    width: '40%',
+		    padding:'0',
+		    fontSize: '1.2em',
+			textAlign: 'center',
+		}
 
-	return (
-	  <div>
-	    <Header />
-	    <div style={imgDiv}>
-	    	<img className="img-responsive" style={{width: '100%', width: '100%'}} src='https://d2d8wwwkmhfcva.cloudfront.net/1000x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5e16a4a1-116c-41ea-9121-33615a3bff26.jpg' />
-	    </div>
-	    <div style={{
-	    	margin: '3%',
-	    	marginLeft: '0%',
-			width: '45%',
-			height: '500px',
-			float: 'right',
-		}}>
+		return (
+		  <div>
+		    <Header />
+		    <div style={{
+		    	marginTop: '3%',
+		    	marginLeft: '25%',
+		    	marginRight: '25%',
+				width: '50%',
+				float: 'left' 
+			}}>
+		    	<img className="img-responsive" style={{width: '100%', width: '100%'}} src='https://d2d8wwwkmhfcva.cloudfront.net/1000x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5e16a4a1-116c-41ea-9121-33615a3bff26.jpg' />
+		    </div>
+		    <div style={{
+		    	margin: '3%',
+				width: '94%',
+				float: 'right',
+			}}>
 
-			<h1 style={{marginTop: '0', fontSize: '2em'}}>Skyy Vodka</h1>
-			<p style={{marginTop: '5%', color: 'grey', fontSize: '1.4em'}} >1.75 LTR</p>
-			{price}
-			<select style={{marginTop: '5%', borderColor: '#CCCCCC', webkitAppearance: 'menulist-button', height: '44px', width: '64px'}}>
-    			<option value="1">1</option>
-    			<option value="2">2</option>
-    			<option value="3">3</option>
-    			<option value="4">4</option>
-    			<option value="5">5</option>
-    			<option value="6">6</option>
-    			<option value="7">7</option>
-    			<option value="8">8</option>
-    			<option value="9">9</option>
-  			</select>
-  			<button className="primary" style={{marginLeft: '5%', height:'46px', width: '40%'}}>Add to Cart</button>
-  			<button className="primaryRedWithHover" style={astext}>
-  				<i class="fa fa-th-list fa-2x" style={{width: '80px'}}/>
-  				Add to List
-  			</button>
-  			<HorizontalScroll items={this.state.savingsItems} title="Similar Items"/>
-	    </div>
-	  </div>
+				<h1 style={{marginTop: '0', fontSize: '2em'}}>Skyy Vodka</h1>
+				<p style={{marginTop: '3%', color: 'grey', fontSize: '1.4em'}} >1.75 LTR</p>
+				{price}
+				<select style={{marginTop: '3%', borderColor: '#CCCCCC', webkitAppearance: 'menulist-button', height: '44px', width: '60%'}}>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+				</select>
+				<button className="primaryRedWithHover" style={astext}>
+					<i class="fa fa-th-list" style={{width: '20%'}}/>
+					Add to List
+				</button>
+				<button className="primary" style={{marginTop: '3%', height:'46px', width: '100%'}}>Add to Cart</button>
+				<HorizontalScroll items={this.state.savingsItems} title="Similar Items"/>
+		    </div>
+		  </div>
+		);
+	} else {
+		const astext = {
+		    background:'none',
+		    border:'none',
+		    width: '80px',
+		    marginLeft: '10%',
+		    padding:'0',
+		    fontSize: '1em',
+			textAlign: 'center',
+		}
+
+		return (
+		  <div>
+		    <Header />
+		    <div style={{
+		    	margin: '3%',
+				width: '45%',
+				height: '500px',
+				float: 'left' 
+			}}>
+		    	<img className="img-responsive" style={{width: '100%', width: '100%'}} src='https://d2d8wwwkmhfcva.cloudfront.net/1000x/filters:fill(FFF,true):format(jpg)/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5e16a4a1-116c-41ea-9121-33615a3bff26.jpg' />
+		    </div>
+		    <div style={{
+		    	margin: '3%',
+		    	marginLeft: '0%',
+				width: '45%',
+				height: '500px',
+				float: 'right',
+			}}>
+
+				<h1 style={{marginTop: '0', fontSize: '2em'}}>Skyy Vodka</h1>
+				<p style={{marginTop: '5%', color: 'grey', fontSize: '1.4em'}} >1.75 LTR</p>
+				{price}
+				<select style={{marginTop: '5%', borderColor: '#CCCCCC', webkitAppearance: 'menulist-button', height: '44px', width: '64px'}}>
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+				</select>
+				<button className="primary" style={{marginLeft: '5%', height:'46px', width: '40%'}}>Add to Cart</button>
+				<button className="primaryRedWithHover" style={astext}>
+					<i class="fa fa-th-list fa-2x" style={{width: '80px'}}/>
+					Add to List
+				</button>
+				<HorizontalScroll items={this.state.savingsItems} title="Similar Items"/>
+		    </div>
+		  </div>
 	);
+	}
   }
 }
 
