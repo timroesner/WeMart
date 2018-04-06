0ee50eeb1cd541095957571d4e822d13b3cbdde0
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2018-04-05 22:54:37 -0700

    Add DB credentials for production

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,36 +18,47 @@ class Home extends Component {
   }
 
   componentDidMount = () =>  {
+
     // Get the dynamoDB database
-        var dynamodb = db;
-
-        // Get the table whose name is "item"
-        var params = {
-            TableName: "item"
-        };
-
-        dynamodb.scan(params, (err, data) => {
-            if (err) {console.log(err, err.stack)} // an error occurred
-            else{
-                data.Items.forEach((element) => {
-
-                    //TODO Clean this up into a one liner.
-                    let departmentid = element.departmentid.N;
-                    let image = element.image.S;
-                    let itemid = (element.itemid.S);
-                    let name = (element.name.S);
-                    let price = (element.price.N);
-                    let quantity = (element.quantity.S);
-                    let sale = (element.sale.N);
-
-                    let testItem = {itemid: itemid, name: name, departmentid: departmentid, image: image, price: price, quantity: quantity, sale: sale};
-                    this.setState({
-                        savingsItems: [...this.state.savingsItems, testItem]
-                    });
-                });
-            }
+    var dynamodb = null;
+    if(process.env.NODE_ENV === 'development'){
+        dynamodb = db;
+    } else {
+        dynamodb = new DynamoDB({
+            region: "us-west-1",
+            credentials: {
+                accessKeyId: process.env.REACT_APP_DB_accessKeyId,
+                secretAccessKey: process.env.REACT_APP_DB_secretAccessKey},
         });
-      }
+    }
+
+    // Get the table whose name is "item"
+    var params = {
+        TableName: "item"
+    };
+
+    dynamodb.scan(params, (err, data) => {
+        if (err) {console.log(err, err.stack)} // an error occurred
+        else{
+            data.Items.forEach((element) => {
+
+                //TODO Clean this up into a one liner.
+                let departmentid = element.departmentid.N;
+                let image = element.image.S;
+                let itemid = (element.itemid.S);
+                let name = (element.name.S);
+                let price = (element.price.N);
+                let quantity = (element.quantity.S);
+                let sale = (element.sale.N);
+
+                let testItem = {itemid: itemid, name: name, departmentid: departmentid, image: image, price: price, quantity: quantity, sale: sale};
+                this.setState({
+                    savingsItems: [...this.state.savingsItems, testItem]
+                });
+            });
+        }
+    });
+  }
 
   render() {
     return (
