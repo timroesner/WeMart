167df497c4672daa2e331c1553e526e2530cc8b6
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-04-06 22:27:18 -0700

    Remove files that do not pertain to ItemGrid component
    Compile issues in production as described by #8 (comment). Removing files that do not pertain to ItemGrid, ItemCard, and Counter components to specialice this branch to only those 3 components.

--- src/Router.js
+++ src/Router.js
@@ -3,24 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 // All pages here
-import SignUp from './SignUp'
-import LogIn from './LogIn'
-import Confirm from './Confirm'
-import PasswordReset from './PasswordReset'
-import ZipCodeCheck from './ZipCodeCheck'
-import Home from './components/Home'
 import ItemGridTest from "./ItemGridTest";
 
 const Router = () => (
   <main>
     <Switch>
         <Route path='/itemgridtest' component={ItemGridTest}/>
-      <Route exact path='/signup' component={SignUp}/>
-      <Route path='/login' component={LogIn}/>
-      <Route path='/confirm' component={Confirm}/>
-      <Route path='/passwordreset' component={PasswordReset}/>
-      <Route path='/home' component={Home} />
-      <Route path='/' component={ZipCodeCheck}/>
     </Switch>
   </main>
 )
