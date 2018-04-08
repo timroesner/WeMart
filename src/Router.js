b1b72259ff3f164895eb14ef54dff36ef3e302e7
Juan Castillo <juancstlm@gmail.com>
2018-04-07 21:31:21 -0700

    Remove files that do not pertain to AccountSettings page
    Fix global stylesheets

--- src/Router.js
+++ src/Router.js
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 
 // All pages here
-import SignUp from './SignUp'
-import LogIn from './LogIn'
-import Confirm from './Confirm'
-import PasswordReset from './PasswordReset'
-import ZipCodeCheck from './ZipCodeCheck'
-import Home from './components/Home'
+import AccountSettings from "./AccountSettings";
+import LogIn from "./LogIn";
 
 const Router = () => (
   <main>
     <Switch>
-      <Route exact path='/signup' component={SignUp}/>
-      <Route path='/login' component={LogIn}/>
-      <Route path='/confirm' component={Confirm}/>
-      <Route path='/passwordreset' component={PasswordReset}/>
-      <Route path='/home' component={Home} />
-      <Route path='/' component={ZipCodeCheck}/>
+      <Route path={'/accountsettings'} component={AccountSettings}/>
+      <Route path={'/login'} component={LogIn}/>
     </Switch>
   </main>
 )
