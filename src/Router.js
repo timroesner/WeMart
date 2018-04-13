3702b9781c03fdefb16a7298e7b83f7b56e47ba5
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-04-13 13:27:34 -0700

    Remove unnecessary files

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+
+import { Route, Switch } from 'react-router-dom';
 
 // All pages here
-import SignUp from './SignUp'
-import LogIn from './LogIn'
-import Confirm from './Confirm'
-import PasswordReset from './PasswordReset'
-import ZipCodeCheck from './ZipCodeCheck'
-import Home from './components/Home'
 import Checkout from "./Checkout";
 
 const Router = () => (
