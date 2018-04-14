86d62696d93247bea2c9f1743f1e74a59e605e11
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-04-14 11:57:24 -0700

    Update to WeMart ic-snacks theme Fix for #10

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route } from 'react-router-dom';
+import {themer} from "ic-snacks";
+import {wemartTheme} from './wemartTheme';
 
 import Router from './Router.js';
+themer.themeConfig = wemartTheme; //IC-Snacks theme for WeMart
 
 
 ReactDOM.render(
