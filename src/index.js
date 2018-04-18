fab5538c924f088883f948bbc6c0e8e3bde073b9
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-04-18 13:46:09 -0700

    Add a wrapper style element to fix #34

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route } from 'react-router-dom';
+import {SetStyles} from "ic-snacks";
+import {StyleRoot} from "radium";
 
 import Router from './Router.js';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Router />
-  </BrowserRouter>, 
+    <StyleRoot>
+        <SetStyles assetsUrl={'./fonts'}/>
+        <BrowserRouter>
+        <Router />
+        </BrowserRouter>
+    </StyleRoot>
+  ,
   document.getElementById('root')
 );
 
