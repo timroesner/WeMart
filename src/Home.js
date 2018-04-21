a559043ee27060d744625631877e3a68d176ce57
rajmakda <makdaraj7@gmail.com>
2018-04-20 20:16:27 -0700

    This commit works on functionality of home page

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,10 @@ import Header from './components/header';
 import { withRouter } from 'react-router-dom';
 import AWS from 'aws-sdk/index';
 import {DynamoDB} from 'aws-sdk/index';
+import {CognitoUserPool} from "amazon-cognito-identity-js";
 
+var poolData;
+var dynamodb;
 
 class Home extends Component {
   constructor(props) {
@@ -13,71 +16,187 @@ class Home extends Component {
     this.state = {
       departmentItems: [],
       savingsItems: [],
-      historyItems: []
+      historyItems: [],
+      isLoggedIn: false,
+      userid: '',
     }
   }
 
-  componentDidMount = () =>  {
-
-    // Get the dynamoDB database
-    var dynamodb;
+  setKeys() {
     if(process.env.NODE_ENV === 'development'){
-        dynamodb = require('./db').db;
-    } else {
-        dynamodb = new DynamoDB({
-            region: "us-west-1",
-            credentials: {
-                accessKeyId: process.env.REACT_APP_DB_accessKeyId,
-                secretAccessKey: process.env.REACT_APP_DB_secretAccessKey},
-        });
-    }
+            poolData =require('./poolData').poolData;
+            dynamodb = require('./db').db;
+        } else{
+            poolData = {
+                UserPoolId : process.env.REACT_APP_Auth_UserPoolId,
+                ClientId : process.env.REACT_APP_Auth_ClientId
+            }
+            dynamodb = new DynamoDB({
+                region: "us-west-1",
+                credentials: {
+                    accessKeyId: process.env.REACT_APP_DB_accessKeyId,
+                    secretAccessKey: process.env.REACT_APP_DB_secretAccessKey},
+            });
+        }
+  }
 
-    // Get the table whose name is "item"
+  getDepartments() {
     var params = {
-        TableName: "item",
-        Limit: 10
+	    TableName: "department",
+      Limit: 10
+	   };
+
+	  var departments = [];
+	  dynamodb.scan(params, (err, data) => {
+	    if (err) {
+	      alert(JSON.stringify(err))
+	    }
+      else {
+        data.Items.forEach((element) => {
+	      departments.push({name: element.departmentid.S, image: element.image.S})
+	      });
+				this.setState({departmentItems: departments})
+	    }
+	  });
+  }
+
+  getSavingsItems() {
+
+    var params = {
+      ExpressionAttributeValues: {
+      ':s': {N: '0'}
+      },
+      FilterExpression: 'sale > :s ',
+      TableName: "item",
+      Limit: 50
     };
 
     dynamodb.scan(params, (err, data) => {
-        if (err) {console.log(err, err.stack)} // an error occurred
-        else{
-            data.Items.forEach((element) => {
+      if (err) {console.log(err, err.stack)} // an error occurred
+      else {
+        data.Items.forEach((element) => {
+          let departmentid = element.department.S;
+          let image = element.image.S;
+          let itemid = (element.itemid.S);
+          let name = (element.name.S);
+          let price = (element.price.N);
+          let quantity = (element.quantity.S);
+          let sale = (element.sale.N);
 
-                //TODO Clean this up into a one liner.
-                let departmentid = element.department.S;
-                let image = element.image.S;
-                let itemid = (element.itemid.S);
-                let name = (element.name.S);
-                let price = (element.price.N);
-                let quantity = (element.quantity.S);
-                let sale = (element.sale.N);
-
-                let testItem = {
-                    itemid: itemid, name: name, departmentid: departmentid, image: image, price: price,
-                    quantity: quantity, sale: sale, inCart: 0};
-                this.setState({
-                    savingsItems: [...this.state.savingsItems, testItem]
-                });
-            });
-        }
+          let saleItem = {
+              itemid: itemid, name: name, departmentid: departmentid, image: image, price: price,
+              quantity: quantity, sale: sale, inCart: 0};
+          this.setState({
+              savingsItems: [...this.state.savingsItems, saleItem]
+          });
+        });
+      }
     });
+  }
 
-    var params = {
-	        TableName: "department",
-          Limit: 10
-	    };
+  getCognitoUser(){
 
-	    var departments = [];
-	    dynamodb.scan(params, (err, data) => {
-	        if (err) {
-	        	alert(JSON.stringify(err))
-	        } else {
-	            data.Items.forEach((element) => {
-	            	departments.push({name: element.departmentid.S, image: element.image.S})
-	            });
-				      this.setState({departmentItems: departments})
-	        }
-	    });
+    var userPool = new CognitoUserPool(poolData);
+    var cognitoUser = userPool.getCurrentUser();
+    console.log(cognitoUser.username);
+    if (cognitoUser != null) {
+      this.setState({isLoggedIn: true, userid: cognitoUser.username})
+    }
+    // if (cognitoUser != null) {
+    //     cognitoUser.getSession(function(err, session) {
+    //         if (err) {
+    //             alert(err);
+    //             return;
+    //         }
+    //     });
+    //     // Necessary because the closure has no access to this.state
+    //     let self = this;
+    //     cognitoUser.getUserAttributes(function(err, result) {
+    //         if (err) {
+    //             alert(err);
+    //             //TODO remove these alerts
+    //             return;
+    //         }
+    //         self.setState({isLoggedIn: true})
+    //         console.log('[Cognito User Attributes]',result) //Logs user attributes
+    //
+    //         result.forEach((attribute) => {
+    //             if(attribute.Name === 'email'){
+    //                 self.setState({user: {...self.state.user , email: attribute.Value}}) // set the email
+    //                 self.setState({user: {...self.state.user , userId: attribute.Value}}) //set the userId
+    //             }
+    //         })
+    //         self.getUserDetails()
+    //     });
+    //
+    // }
+    }
+    getUsersOrderHistory(){
+        var userParams = {
+            Key: {
+                'userid': {S: this.state.userId}
+            },AttributesToGet: [
+                'history'
+                /* more items */
+            ],
+            TableName: "user"
+        };
+        dynamodb.getItem(userParams, (err, data) => {
+            if(err) {console.log(err, err.stack)}
+            else{
+                data.Item.history.L.forEach((order)=>{
+                    order.M.items.L.forEach((i)=>{
+                        let item = i
+                        console.log('[item]',item)
+                        var historyItems = []
+                        historyItems.push(item)
+                        this.setState({historyItems: historyItems})
+                    })
+                })
+            }
+        })
+    }
+
+  // getHistoryItems() {
+  //   this.state.orderHistory.forEach((itemid)=>{
+  //           console.log('itemid',itemid)
+  //           var itemParams  = {
+  //               Key: {'itemid': {S:itemid}},
+  //               TableName: 'item'
+  //           }
+  //
+  //           dynamodb.getItem(itemParams,(err, data)=>{
+  //               if(err) {console.log(err, err.stack)}
+  //               else if(data.Item) {
+  //                   console.log(data);
+  //                   let image = data.Item.image.S;
+  //                   let itemid = data.Item.itemid.S;
+  //                   let name = data.Item.name.S;
+  //                   let price = (data.Item.price.N);
+  //                   let quantity = (data.Item.quantity.S);
+  //                   let sale = (data.Item.sale.N);
+  //
+  //                   let testItem = {
+  //                       key: itemid, itemid: itemid, name: name, image: image, price: price,
+  //                       quantity: quantity, sale: sale, inCart: 0};
+  //                   this.setState({
+  //                       items: [...this.state.items, testItem]
+  //                   });
+  //
+  //                   console.log('test item', testItem)
+  //               }
+  //           })
+  //       })
+  // }
+
+  componentDidMount = () =>  {
+
+    this.setKeys()
+    this.getCognitoUser()
+    this.getDepartments()
+    this.getSavingsItems()
+    this.getUsersOrderHistory()
+
   }
 
 
@@ -87,8 +206,8 @@ class Home extends Component {
         <Header />
         <div id="pageBody" className="container-fluid">
           <HorizontalScroll items={this.state.departmentItems} title="Browse by Department"/>
-          <HorizontalScroll items={this.state.savingsItems} title="History"/>
           <HorizontalScroll items={this.state.savingsItems} title="Savings"/>
+          <HorizontalScroll items={this.state.historyItems} title="History"/>
       </div>
       </div>
     );
