9d0fc7b9c33d8a4c46db7407c44964607f29079a
timroesner <13894518+timroesner@users.noreply.github.com>
2018-04-21 22:11:03 -0700

    URI encode Department name

--- src/Search.js
+++ src/Search.js
@@ -23,6 +23,7 @@ class Search extends Component {
 		const queryParams = new URLSearchParams(this.props.location.search);
     	query = queryParams.get('query')
     	let special = queryParams.get('special')
+    	alert(query)
 
     	if(special == "true") {
     		if(query == "savings") {
