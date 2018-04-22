b426f37970a6c1fce0f49f3f5f7551c06a518dba
Juan Castillo <juan.a.castillo@sjsu.edu>
2018-04-21 23:22:01 -0700

    Remove global background color styling

--- src/components/ProfilePanel.js
+++ src/components/ProfilePanel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 //STYLES
-const profilePanel = {backgroundColor:'#FFFFFF',border:'1px solid #F0EFEC',borderRadius:'.6rem',
+const profilePanel = {backgroundColor:'#FFFFFF',border:'1px solid #d30707',borderRadius:'.6rem',
 marginBottom:'2rem',marginRight:'2rem',marginLeft:'2rem',}
 const profilePanel_h4 = {fontWeight:'800',fontSize:'1.8rem',lineHeight:'1.8rem',padding:'2rem 3rem',margin:'0'}
 
